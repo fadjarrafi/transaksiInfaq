6b8301fa0e7c4457133d94737332576a6f7f836e
fadjarrafi <dark.fir21@gmail.com>
2019-12-18 04:50:20 +0700

    penambahan fungsi readAll pada TabungaRepository

--- TransaksiInfaq/Model/Repository/TabunganRepository.cs
+++ TransaksiInfaq/Model/Repository/TabunganRepository.cs
@@ -55,15 +55,15 @@ namespace TransaksiInfaq.Model.Repository
             int result = 0;
 
             // deklarasi perintah SQL
-            string sql = @"update Tabungan set No_Rekening=@No_Rekening, Bank=@Bank, Saldo=@Saldo";
+            string sql = @"update Tabungan set Bank=@Bank, Saldo=@Saldo where No_Rekening=@No_Rekening";
 
             // membuat objek command menggunakan blok using
             using (OleDbCommand cmd = new OleDbCommand(sql, _conn))
             {
                 // mendaftarkan parameter dan mengeset nilainya
-                cmd.Parameters.AddWithValue("@No_Rekering", tbg.No_rekening);
                 cmd.Parameters.AddWithValue("@Bank", tbg.Bank);
                 cmd.Parameters.AddWithValue("@Saldo", tbg.Saldo);
+                cmd.Parameters.AddWithValue("@No_Rekering", tbg.No_rekening);
 
                 try
                 {
@@ -104,6 +104,46 @@ namespace TransaksiInfaq.Model.Repository
             }
 
             return result;
+        }
+
+        public List<Tabungan> ReadAll()
+        {
+            // membuat objek collection untuk menampung objek mahasiswa
+            List<Tabungan> list = new List<Tabungan>();
+
+            try
+            {
+                // deklarasi perintah SQL
+                string sql = @"select * from Tabungan 
+                        order by No_rekening";
+
+                // membuat objek command menggunakan blok using
+                using (OleDbCommand cmd = new OleDbCommand(sql, _conn))
+                {
+                    // membuat objek dtr (data reader) untuk menampung result set (hasil perintah SELECT)
+                    using (OleDbDataReader dtr = cmd.ExecuteReader())
+                    {
+                        // panggil method Read untuk mendapatkan baris dari result set
+                        while (dtr.Read())
+                        {
+                            // proses konversi dari row result set ke object
+                            Tabungan tbg = new Tabungan();
+                            tbg.No_rekening = dtr["No_rekening"].ToString();
+                            tbg.Bank = dtr["Bank"].ToString();
+                            tbg.Saldo = dtr["Saldo"].ToString();
+
+                            // tambahkan objek mahasiswa ke dalam collection
+                            list.Add(tbg);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print("ReadAll error: {0}", ex.Message);
+            }
+
+            return list;
         }
     }
 }
